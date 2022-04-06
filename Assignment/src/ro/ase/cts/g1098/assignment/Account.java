9d48d668d014869bdeb1b7c9841d420c837646e6
MatacheTeodor <matacheteodor19@stud.ase.ro>
2022-04-06 21:16:58 +0300

    Created separate class for 'calculate' function

--- Assignment/src/ro/ase/cts/g1098/assignment/Account.java
+++ Assignment/src/ro/ase/cts/g1098/assignment/Account.java
@@ -3,23 +3,26 @@ package ro.ase.cts.g1098.assignment;
 public class Account {
 	public double loan_value, rate;
 	public int daysActive, account_Type;
-	public static final int STANDARD = 0, BUDGET = 1, PREMIUM = 2, SUPER_PREMIUM = 3;
+	public static final int	STANDARD=0,BUDGET=1,PREMIUM=2,SUPER_PREMIUM=3;
 
 	//This method returns the value of the loan
 	public double loan() {
 		System.out.println("The loan value is " + this.loan_value);
 		return loan_value;
 	}
+	
     //This method called getRate, takes in one argument(the variable named 'rent') and returns its value
 	public double getRate() {
 		System.out.println("The rate is " + rate);
 		return this.rate;
 	}
+	
      //This is a method called getMonthlyRate that calculates the monthly rate for our loan and returns it as a double.
 	// must have method - the lead has requested it in all classes
 	public double getMonthlyRate() {
 		return loan_value * rate;
 	}
+	
     //Next, we have setValue which sets the value of our loan to some number if it's positive
 	//or throws an exception if it's negative.
 	public void setValue(double value) throws Exception {
@@ -39,20 +42,7 @@ public class Account {
 		int vb = 10;
 		System.out.println("This is an account");
 	}
-	//This calculates the total fee of an account
-	public static double calculate(Account[] accounts) {
-		double totalFee = 0.0;
-		Account account;
-		int temp = 365;
-		for (int i = 0; i < accounts.length; i++) {
-			account = accounts[i];
-			if (account.account_Type == Account.PREMIUM || account.account_Type == Account.SUPER_PREMIUM)
-				totalFee += .0125 * ( // 1.25% broker's fee
-				account.loan_value * Math.pow(account.rate, (account.daysActive / 365)) - account.loan_value); // interest-principal
-		}
-		return totalFee;
-	}
-
+	
 	public Account(double value, double rate, int account_Type) throws Exception {
 		if (value < 0)
 			throw new Exception();
---
+++ Assignment/src/ro/ase/cts/g1098/assignment/CalculateFee.java
@@ -0,0 +1,17 @@
+package ro.ase.cts.g1098.assignment;
+
+public class CalculateFee {
+	//This calculates the total fee of an account
+		public static double calculate(Account[] accounts) {
+			double totalFee = 0.0;
+			Account account;
+			int temp = 365;
+			for (int i = 0; i < accounts.length; i++) {
+				account = accounts[i];
+				if (account.account_Type == Account.PREMIUM || account.account_Type == Account.SUPER_PREMIUM)
+					totalFee += .0125 * ( // 1.25% broker's fee
+					account.loan_value * Math.pow(account.rate, (account.daysActive / 365)) - account.loan_value); // interest-principal
+			}
+			return totalFee;
+		}
+}
