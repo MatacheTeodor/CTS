3eab35ea775f51cd6b55106481780f00882edee7
MatacheTeodor <matacheteodor19@stud.ase.ro>
2022-04-06 19:34:30 +0300

    Update Account.java

--- Assignment/src/ro/ase/cts/g1098/assignment/Account.java
+++ Assignment/src/ro/ase/cts/g1098/assignment/Account.java
@@ -5,21 +5,23 @@ public class Account {
 	public int daysActive, account_Type;
 	public static final int STANDARD = 0, BUDGET = 1, PREMIUM = 2, SUPER_PREMIUM = 3;
 
+	//This method returns the value of the loan
 	public double loan() {
 		System.out.println("The loan value is " + this.loan_value);
 		return loan_value;
 	}
-
+    //This method called getRate, takes in one argument(the variable named 'rent') and returns its value
 	public double getRate() {
 		System.out.println("The rate is " + rate);
 		return this.rate;
 	}
-
+     //This is a method called getMonthlyRate that calculates the monthly rate for our loan and returns it as a double.
 	// must have method - the lead has requested it in all classes
 	public double getMonthlyRate() {
 		return loan_value * rate;
 	}
-
+    //Next, we have setValue which sets the value of our loan to some number if it's positive
+	//or throws an exception if it's negative.
 	public void setValue(double value) throws Exception {
 		if (value < 0)
 			throw new Exception();
@@ -37,7 +39,7 @@ public class Account {
 		int vb = 10;
 		System.out.println("This is an account");
 	}
-
+	//This calculates the total fee of an account
 	public static double calculate(Account[] accounts) {
 		double totalFee = 0.0;
 		Account account;
